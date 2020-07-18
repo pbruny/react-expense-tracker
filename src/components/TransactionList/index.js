c9c7fb3338155e1aa32d97bba2d0febcf18755af
Paulo Lima <paulobruny@gmail.com>
2020-07-18 19:40:01 -0300

    Update TransactionList

--- src/components/TransactionList/index.js
+++ src/components/TransactionList/index.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import Transaction from '../Transaction'
+import { GlobalContext } from '../../context/GlobalState'
 
 const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext)
+
   return (
     <>
       <h3>History</h3>
       <ul className="list" id="list">
-        <li className="minus">Cash <span>-$400.00</span><button className="delete-btn">x</button></li>
+        {transactions.map(transaction => {
+          return (
+            <Transaction key={transaction.id} transaction={transaction} />
+          )
+        })}
       </ul>
     </>
   )
