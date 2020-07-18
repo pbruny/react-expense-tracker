ecf1ef0cef15e03652ebc46f04e640fc459bef01
Paulo Lima <paulobruny@gmail.com>
2020-07-18 20:22:58 -0300

    add transaction dinamically

--- src/components/AddTransaction/index.js
+++ src/components/AddTransaction/index.js
@@ -1,13 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import { GlobalContext } from '../../context/GlobalState'
+
 
 const AddTransaction = () => {
   const [description, setDescription] = useState('')
   const [value, setValue] = useState(0)
 
+  const { addTransaction } = useContext(GlobalContext)
+
+  const formSubmit = e => {
+    e.preventDefault()
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 1000000000),
+      description,
+      value: Number(value)
+    }
+
+    addTransaction(newTransaction)
+    setValue(0)
+    setDescription('')
+  }
+
   return (
     <>
       <h3>Add new transaction</h3>
-      <form>
+      <form onSubmit={formSubmit} >
         <div className="form-control">
           <label htmlFor="text">Add a description</label>
           <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Enter description" />
