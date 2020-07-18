7a9d7a7b99a0d261dac5d20d64a70763c0160a0d
Paulo Lima <paulobruny@gmail.com>
2020-07-18 19:56:07 -0300

    calculate balance

--- src/components/Balance/index.js
+++ src/components/Balance/index.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../../context/GlobalState'
 
 const Balance = () => {
+  const { transactions } = useContext(GlobalContext)
+
+  const values = transactions.map(transaction => transaction.value)
+
+  const total = values.reduce((acc, value) => {
+    return acc += value
+  }, 0).toFixed(2)
+
   return (
     <>
       <h4>Your balance</h4>
-      <h1 id="balance">$0.00</h1>
+      <h1 id="balance">${total}</h1>
     </>
   )
 }
