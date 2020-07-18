f065fbaea3f6a0b3957ffdcbad225d4fd8ed0a40
Paulo Lima <paulobruny@gmail.com>
2020-07-18 20:23:13 -0300

    delete transaction funcionality

--- src/components/Transaction/index.js
+++ src/components/Transaction/index.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../../context/GlobalState'
+
 
 const Transaction = ({ transaction }) => {
+  const { deleteTransaction } = useContext(GlobalContext)
+
   return (
     <li className={transaction.value > 0 ? 'plus' : 'minus'}>
       {transaction.description} 
       <span>
         {transaction.value > 0 ? `+$${Math.abs(transaction.value)}` : `- $${Math.abs(transaction.value)}`}
       </span>
-      <button className="delete-btn">x</button>
+      <button className="delete-btn" onClick={() => deleteTransaction(transaction.id)}>x</button>
     </li>
   )
 }
