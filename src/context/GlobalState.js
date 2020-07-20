2fd93a62d9aaf15dd7e5771d084ccc0c4e676b8a
Paulo Lima <paulobruny@gmail.com>
2020-07-20 19:49:08 -0300

    add localStorage

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,14 +1,18 @@
-import React, { createContext, useReducer } from 'react'
+import React, { createContext, useReducer, useEffect } from 'react'
 import AppReducer from './AppReducer'
 
 const initialState = {
-  transactions: []
+  transactions: JSON.parse(localStorage.getItem('transactions')) || []
 }
 
 export const GlobalContext = createContext(initialState)
 
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState)
+
+  useEffect(() => {
+    localStorage.setItem('transactions', JSON.stringify(state.transactions))
+  })
 
   const deleteTransaction = (id) => {
     dispatch({
